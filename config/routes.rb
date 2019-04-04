68c24c253b659db03292d3d1a58ffb44fbfd415d
Emily Johnson <emily.johnsonmsp@gmail.com>
2019-04-03 20:00:41 -0600

    add rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'email'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users
+    resources :courses
+  end
+
 end
