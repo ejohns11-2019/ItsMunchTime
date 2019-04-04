04b02732ff2e04dd3eefc5c090ac539f4c1321a6
Jonny Fluckey <46408080+jonnyfluckey@users.noreply.github.com>
2019-04-03 20:30:13 -0600

    Revert "add rails routes"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'email'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    resources :users
-    resources :courses
-  end
-
 end
