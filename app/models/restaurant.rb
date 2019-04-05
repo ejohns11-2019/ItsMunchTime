359832913970dd7083db530a857fb374ae983c24
Emily Johnson <emily.johnsonmsp@gmail.com>
2019-04-04 19:11:25 -0600

    restaurant and order model validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :orders
   has_many :users, through: :orders
+
+  validates :name, length: { 1..50 }, presence :true, on: :create
+  validates :address, uniqueness: true
+  validates :phone, length: { is: 10 }
+  validates :menu, allow_blank: true
 end
