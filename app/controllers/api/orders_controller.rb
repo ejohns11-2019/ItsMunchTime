526c08e63ac1a3cb7285db8a3e6aeca6b459a5d7
David Sarkisian <dsarkisian@plansource.com>
2019-04-04 19:22:22 -0600

    Finished draft Order Controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -4,29 +4,42 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    order = current_user.orders.new(order_params)
-    if item.save
-      render json: item
-    else
-      render json: { errors: item.errors }, status: :unprocessable_entity 
+    if current_user.adimn == true  
+      users = User.find(params[:group] == full_time)
+      users.each do |user|
+        order = user.orders.new(order_params) 
+        if order.save
+          render json: order
+        else
+          render json: { errors: order.errors }, status: :unprocessable_entity 
+        end
+      end
     end
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(complete: !item.complete)
-    render json: item
+    if current_user.admin == false
+      order = current_user.orders.find(params[:id])
+      order.update(order_params)
+      render json: order
+    else
+      #add logic for admin user to be able to update any users order
+      regular_user = User.find([:id])
+      order = regular_user.orders.find(params[:id])
+      order.update(order_params)
+      render json: order 
+    end
   end
 
   def destroy
-    Item.find(params[:id]).destroy
-    render json: { message: 'Item deleted' }
+    if current_user.adimn == true  
+      User.each { |user|  user.ordres.find(params[:id]).destroy}
+      render json: { message: 'Order was deleted deleted' }
+    end
   end
 
   private
-
-  def item_params
-    params.require(:order).permit(:name, :ticket, :current, :order_date)
-  end
-
+    def order_params
+      params.require(:order).permit(:user_id, :ticket, :restaurant_id, :current, :order_date)
+    end 
 end
