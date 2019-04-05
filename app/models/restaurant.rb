4b95685930838dd6ee5f3f36d11d75b574fed077
Emily Johnson <emily.johnsonmsp@gmail.com>
2019-04-04 19:44:52 -0600

    fix umodel validations and migration duplicate

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :orders
 
   validates :name, length: { 1..50 }, presence :true, on: :create
-  validates :address, uniqueness: true
+  validates :address, uniqueness: true, length: { maximum: 100 }
   validates :phone, length: { is: 10 }
-  validates :menu, allow_blank: true
+  validates :menu, allow_blank: true, length: { maximum: 500 }
 end
