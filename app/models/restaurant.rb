bb289d51013fb6afc9d4b316e285a61467a91fce
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-04-07 15:24:11 -0600

    commented out validation for testing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,8 @@ class Restaurant < ApplicationRecord
   has_many :orders
   has_many :users, through: :orders
 
-  validates :name, length: { 1..50 }, presence :true, on: :create
-  validates :address, uniqueness: true, length: { maximum: 100 }
-  validates :phone, length: { is: 10 }
-  validates :menu, allow_blank: true, length: { maximum: 500 }
+  # validates :name, length: { 1..50 }, presence :true, on: :create
+  # validates :address, uniqueness: true, length: { maximum: 100 }
+  # validates :phone, length: { is: 10 }
+  # validates :menu, allow_blank: true, length: { maximum: 500 }
 end
