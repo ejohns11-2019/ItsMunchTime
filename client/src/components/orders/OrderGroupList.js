b3c37f9c53d06c5b75b45aa462adee74a80cd133
David Sarkisian <dsarkisian@plansource.com>
2019-04-10 20:19:02 -0600

    Completed order form for admin

--- client/src/components/orders/OrderGroupList.js
+++ client/src/components/orders/OrderGroupList.js
@@ -3,17 +3,13 @@ import Order from './Order'
 
 //Logical component that will handle order display, and all order CRUD actions
 
-class OrderList extends Component {
+const OrderList = () => (
 
-  render() {
+  <>
+    <h2>This is the OrderList Component</h2>
+    <Order />
+  </>
 
-    return (
-      <>
-      <h2>This is the OrderList Component</h2>
-      <Order />
-      </>
-    )
-  }
-}
+)
 
 export default OrderList;
